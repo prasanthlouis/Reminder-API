ab96154b38f8a196cea17c96d2798fcff1848f8f
prasanthlouis <prasanthlouis21@gmail.com>
2015-10-02 10:45:39 -0400

    error commit

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 #!flask/bin/python
 from flask import Flask, jsonify
+from flask import abort
+from flask import make_response
 
 app = Flask(__name__)
 
-tasks = [
+reminders = [
     {
         'id': 1,
         'title': u'Market',
@@ -24,16 +26,23 @@ tasks = [
     }
 
 ]
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
 @app.route('/todo/api/v1/task/<int:task_id>', methods=['GET'])
 def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
+    task = [task for task in reminders if task['id'] == task_id]
     if len(task) == 0:
         abort(404)
     return jsonify({'task': task[0]})
 
 @app.route('/todo/api/v1/task', methods=['GET'])
-def get_tasks():
-    return jsonify({'listoftasks': tasks})
+def get_reminders():
+    return jsonify({'listofreminders': reminders})
 
 if __name__ == '__main__':
     app.run(debug=True)
