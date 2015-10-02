6dc007414899a969284133e75e97288b33904de5
prasanthlouis <prasanthlouis21@gmail.com>
2015-10-02 11:39:10 -0400

    delete commit

--- app.py
+++ app.py
@@ -36,6 +36,24 @@ def delete_task(task_id):
     reminders.remove(task[0])
     return jsonify({'result': True})
 
+@app.route('/todo/api/v1/task/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = [task for task in reminders if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})   
+
 @app.route('/todo/api/v1/task', methods=['POST'])
 def create_task():
     if not request.json or not 'title' in request.json:
