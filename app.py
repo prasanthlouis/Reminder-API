5d84723a8e27dcf17677781c9da78933f5305150
prasanthlouis <prasanthlouis21@gmail.com>
2015-10-02 10:37:06 -0400

    id commit

--- app.py
+++ app.py
@@ -24,6 +24,12 @@ tasks = [
     }
 
 ]
+@app.route('/todo/api/v1/task/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
 
 @app.route('/todo/api/v1/task', methods=['GET'])
 def get_tasks():
